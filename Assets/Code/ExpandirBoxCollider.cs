85b3501d2b748d053ce60c0f2d4a2720e1b841e3
AbrilMontenegro0412 <montenegroperez9@gmail.com>
2025-09-25 18:09:16 -0300

    cosas varias
    laberinto y casa

---
+++ Assets/Code/ExpandirBoxCollider.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+public class ExpandirBoxCollider : MonoBehaviour
+{
+    //Start is called once before the first execution of Update after the MonoBehaviour is created
+
+    //CODIGO COMPLETAMENTE PROVISORIO (ARREGLAR CAMARA)
+    void Start()
+    {
+        BoxCollider[] colliders = Object.FindObjectsByType<BoxCollider>(FindObjectsSortMode.None);
+
+        foreach (BoxCollider col in colliders)
+        {
+            Vector3 size = col.size;
+            size.x += 0.5f;
+            size.y += 0.5f;
+            size.z += 0.5f;
+            col.size = size;
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
