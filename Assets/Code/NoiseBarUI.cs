7c7e347549a47d4f7228004089086a499313d759
Jesús Briceño <jebp2389@gmail.com>
2025-09-28 16:19:58 -0300

    LAst last changes

--- Assets/Code/NoiseBarUI.cs
+++ Assets/Code/NoiseBarUI.cs
@@ -1,24 +1,17 @@
-using Unity.VisualScripting;
-using UnityEditor.ShaderKeywordFilter;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class NoiseBarUI : MonoBehaviour
 {
-    [SerializeField] private Slider noiseSlider;
-    [SerializeField] private Image fillImage;
+    [SerializeField] Slider noiseSlider;
+    [SerializeField] Image fillImage;
 
-    private void OnEnable() => GameEvents.OnNoiseChanged += UpdateNoise;
-    private void OnDisable() => GameEvents.OnNoiseChanged -= UpdateNoise;
+    void OnEnable() { GameEvents.OnNoiseChanged += UpdateNoise; }
+    void OnDisable() { GameEvents.OnNoiseChanged -= UpdateNoise; }
 
-    private void UpdateNoise(float normalizedValue)
+    void UpdateNoise(float normalizedValue)
     {
-        if (noiseSlider != null)
-            noiseSlider.value = normalizedValue;
-
-        if (fillImage != null)
-            fillImage.color = new Color32(0xA4, 0x20, 0x20, 0xFF);
-
-        //Debug.Log("Value of the noise: " + normalizedValue);
+        if (noiseSlider) noiseSlider.value = normalizedValue;
+        if (fillImage) fillImage.color = new Color32(0xA4, 0x20, 0x20, 0xFF);
     }
 }
