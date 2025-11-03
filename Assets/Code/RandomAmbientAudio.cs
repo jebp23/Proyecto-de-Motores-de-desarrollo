3a7bcd4dbc37676fe799c07213281875254a390a
jebp23 <jebp2389@gmail.com>
2025-11-02 22:19:57 -0300

    SFX extra

---
+++ Assets/Code/RandomAmbientAudio.cs
@@ -0,0 +1,56 @@
+using UnityEngine;
+
+public class RandomAmbientAudio : MonoBehaviour
+{ 
+    [SerializeField] AudioClip[] ambientClips;
+    [SerializeField] float minDelay = 5f;
+    [SerializeField] float maxDelay = 15f;
+    [SerializeField] float minVolume = 0.8f;
+    [SerializeField] float maxVolume = 1f;
+    [SerializeField] bool playOnStart = true;
+    [SerializeField] bool loopContinuously = true;
+
+    AudioSource source;
+    float nextPlayTime;
+
+    void Awake()
+    {
+        source = GetComponent<AudioSource>();
+        if (!source) source = gameObject.AddComponent<AudioSource>();
+        source.playOnAwake = false;
+        source.spatialBlend = 0f; // 0 = 2D, 1 = 3D
+    }
+
+    void Start()
+    {
+        if (playOnStart)
+            ScheduleNextSound();
+    }
+
+    void Update()
+    {
+        if (!loopContinuously) return;
+
+        if (Time.time >= nextPlayTime)
+        {
+            PlayRandomClip();
+            ScheduleNextSound();
+        }
+    }
+
+    void PlayRandomClip()
+    {
+        if (ambientClips == null || ambientClips.Length == 0) return;
+
+        int index = Random.Range(0, ambientClips.Length);
+        source.clip = ambientClips[index];
+        source.volume = Random.Range(minVolume, maxVolume);
+        source.Play();
+    }
+
+    void ScheduleNextSound()
+    {
+        float delay = Random.Range(minDelay, maxDelay);
+        nextPlayTime = Time.time + delay;
+    }
+}
