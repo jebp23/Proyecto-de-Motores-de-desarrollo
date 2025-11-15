de7851578789a74dee536c54b15449f75a31e8b6
jebp23 <jebp2389@gmail.com>
2025-11-14 21:50:46 -0300

    final fix

--- Assets/Code/ControlVolumen.cs
+++ Assets/Code/ControlVolumen.cs
@@ -1,36 +1,48 @@
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.Audio;
 
 public class ControlVolumen : MonoBehaviour
 {
     public Slider slider;
-    public float sliderValue;
     public Image muteImage;
+    public AudioMixer mixer;
+
+    private const string PARAM = "MasterVol";
+    private const string SAVE_KEY = "MasterVolValue";
 
     void Start()
     {
-        slider.value = PlayerPrefs.GetFloat("audioVolume", 0.5f);
-        AudioListener.volume = slider.value;
-        CheckMute();
+        float savedDb = PlayerPrefs.GetFloat(SAVE_KEY, -10f);
+        mixer.SetFloat(PARAM, savedDb);
+
+        slider.value = LinearFromDb(savedDb);
+        UpdateMuteIcon();
     }
 
     public void ChangeSlider(float value)
     {
-        sliderValue = value;
-        PlayerPrefs.SetFloat("audioVolume", sliderValue);
-        AudioListener.volume = slider.value;
-        CheckMute();
+        float db = DbFromLinear(value);
+
+        mixer.SetFloat(PARAM, db);
+        PlayerPrefs.SetFloat(SAVE_KEY, db);
+
+        UpdateMuteIcon();
     }
 
-    public void CheckMute()
+    private void UpdateMuteIcon()
     {
-        if (sliderValue == 0)
-        {
-            muteImage.enabled = true;
-        }
-        else
-        {
-            muteImage.enabled = false;
-        }
+        muteImage.enabled = slider.value <= 0.001f;
+    }
+
+    private float DbFromLinear(float v)
+    {
+        v = Mathf.Clamp(v, 0.0001f, 1f);
+        return Mathf.Log10(v) * 20f;
+    }
+
+    private float LinearFromDb(float db)
+    {
+        return Mathf.Pow(10f, db / 20f);
     }
 }
